69555e5ffc0fcfa123bb7304f782776c2fbe1949
M865-byte <72695407+M865-byte@users.noreply.github.com>
2020-10-14 21:11:53 +0530

    Add files via upload

---
+++  Demo.java
@@ -0,0 +1,10 @@
+class Demo
+{
+public static void main(String args[])
+{
+System.out.println("Hello world");
+}
+}
+java location and java path
+
+C:\Program Files (x86)\Common Files\Oracle\Java\javapath;
