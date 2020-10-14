db35acdab4eb3b5cf27d511988252e92d71f3249
Monika T M <72695407+Monika2763@users.noreply.github.com>
2020-10-14 21:35:47 +0530

    sum.java

---
+++ Sum.java
@@ -0,0 +1,21 @@
+class Sum
+{
+	public static void main(String args[])
+        {
+	System.out.println("Hello");
+	int a = 10;
+	int b = 5;
+	int c = 0;
+	System.out.println("value of a is "+a);
+ 	System.out.println( "value of b is "+b);
+        System.out.println(c);
+        c = a + b;
+
+	System.out.println(c);
+	String s1 = "moni";
+	String s2 = s1 + a;
+	//String s3 = s1 + s2;
+	System.out.println(s2);
+	
+         }
+}
